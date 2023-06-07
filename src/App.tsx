249c80a33b7a569b307743dca6b5df2ec17d5856
PetrusAriaa <petrusariacr25@gmail.com>
2023-06-07 12:38:02 +0700

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Store from './routes/Store';
+import Book from './routes/Book';
+import AddBook from './routes/AddBook';
 
 export default function App() {
 	return (
@@ -13,6 +15,14 @@ export default function App() {
 				<Route
 					path='/:storeId'
 					Component={Store}
+				/>
+				<Route
+					path='/:storeId/books/:bookName'
+					Component={Book}
+				/>
+				<Route
+					path='/:storeId/add-book'
+					Component={AddBook}
 				/>
 			</Routes>
 		</Router>
