7cb1d59f4cdd9cd0d26a99582a3a7ef5568afd98
PetrusAriaa <petrusariacr25@gmail.com>
2023-06-07 16:47:07 +0700

    send request POST

--- src/routes/AddBook.tsx
+++ src/routes/AddBook.tsx
@@ -1,34 +1,79 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { BACKEND_PORT } from '../config';
+import axios from 'axios';
 
 const AddBook = () => {
 	const [judul, setJudul] = useState('');
-	const [penulis, setPenulis] = useState('');
-	const [penerbit, setPenerbit] = useState('');
 	const [halaman, setHalaman] = useState('0');
 	const [qty, setQty] = useState('1');
 	const [harga, setHarga] = useState('0');
+	const [tahunPublikasi, setTahunPublikasi] = useState('');
+	const [lPenulis, setlPenulis] = useState<any>([]);
+	const [lPublisher, setlPublisher] = useState<any>([]);
+	const [penulis, setPenulis] = useState('');
+	const [penerbit, setPenerbit] = useState('');
 
-	const handleSubmit = (event: any) => {
+	const params = useParams();
+	const storeId = params.storeId;
+
+	const listPenulis = async () => {
+		const _listPenulis = await fetch(`http://${BACKEND_PORT}/authors`);
+		const jsonPenulis = await _listPenulis.json();
+		setlPenulis(jsonPenulis.items);
+		console.log(jsonPenulis.items);
+		setPenulis(jsonPenulis.items[0].author_name);
+	};
+	const listPublisher = async () => {
+		const _listPublisher = await fetch(`http://${BACKEND_PORT}/publishers`);
+		const jsonPublisher = await _listPublisher.json();
+		setlPublisher(jsonPublisher.items);
+		console.log(jsonPublisher.items);
+		setPenerbit(jsonPublisher.items[0].publisher_name);
+	};
+
+	useEffect(() => {
+		listPenulis();
+		listPublisher();
+	}, []);
+
+	const handleSubmit = async (event: React.FormEvent) => {
 		event.preventDefault();
-		const data = {
-			judul: judul,
-			penulis: penulis,
-			penerbit: penerbit,
-			halaman: halaman,
-			qty: qty,
-			harga: harga,
-		};
-		console.log(data);
-		setJudul('');
-		setPenulis('');
-		setPenerbit('');
-		setHalaman('');
-		setQty('');
-		setHarga('');
+
+		try {
+			const data = {
+				store: storeId,
+				book_name: judul,
+				publication_year: tahunPublikasi,
+				pages: halaman,
+				pname: penerbit,
+				quantity: qty,
+				price: harga,
+				penulis: penulis,
+			};
+			console.log(data);
+			axios.post(`http://${BACKEND_PORT}/${storeId}/books`, data).then(
+				(response) => {
+					console.log(response);
+				},
+				(error) => {
+					console.log(error.response.data);
+				}
+			);
+			setJudul('');
+			setPenulis(lPenulis[0].author_name);
+			setPenerbit(lPublisher[0].publisher_name);
+			setHalaman('0');
+			setQty('1');
+			setHarga('0');
+			setTahunPublikasi('');
+		} catch (err) {
+			console.error('Error:', err);
+		}
 	};
 
 	return (
-		<div className='flex flex-row p-10 border justify-center'>
+		<div className='flex flex-row p-10 justify-center'>
 			<form onSubmit={handleSubmit}>
 				<div className='relative z-0 w-full mb-6 group'>
 					<input
@@ -41,42 +86,61 @@ const AddBook = () => {
 						}}
 						value={judul}
 					/>
-					<label className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
+					<label className='peer-focus:font-medium absolute text-sm text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
 						Judul Buku
 					</label>
 				</div>
 				<div className='relative z-0 w-full mb-6 group'>
-					<input
-						type='text'
-						className='block py-2.5 px-0 w-full text-sm text-slate-500 bg-transparent border-0 border-b-2 border-gray-600 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-						placeholder='dropdown'
-						required
+					<label className='block mb-2 text-sm font-medium text-gray-400 '>
+						Nama Penulis
+					</label>
+					<select
 						onChange={(e) => {
 							setPenulis(e.target.value);
 						}}
 						value={penulis}
-					/>
-					<label className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
-						Nama Penulis
-					</label>
+						id='countries'
+						className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 '>
+						{lPenulis.map((penulis: any, index: any) => (
+							<option key={index}> {penulis.author_name} </option>
+						))}
+					</select>
 				</div>
 				<div className='relative z-0 w-full mb-6 group'>
-					<input
-						type='text'
-						className='block py-2.5 px-0 w-full text-sm text-slate-500 bg-transparent border-0 border-b-2 border-gray-600 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-						placeholder='dropdown'
-						required
+					<label className='block mb-2 text-sm font-medium text-gray-400 '>
+						Nama Penerbit
+					</label>
+					<select
 						onChange={(e) => {
 							setPenerbit(e.target.value);
 						}}
 						value={penerbit}
-					/>
-					<label className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
-						Nama Penerbit
-					</label>
+						id='countries'
+						className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 '>
+						{lPublisher.map((publisher: any, index: any) => (
+							<option key={index}>
+								{publisher.publisher_name}
+							</option>
+						))}
+					</select>
 				</div>
 
 				<div className='grid md:grid-cols-2 md:gap-6'>
+					<div className='relative z-0 w-full mb-6 group'>
+						<input
+							type='text'
+							className='block py-2.5 px-0 w-full text-sm text-slate-500 bg-transparent border-0 border-b-2 border-gray-600 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer'
+							placeholder=' '
+							required
+							onChange={(e) => {
+								setTahunPublikasi(e.target.value);
+							}}
+							value={tahunPublikasi}
+						/>
+						<label className='peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
+							Tahun Publikasi
+						</label>
+					</div>
 					<div className='relative z-0 w-full mb-6 group'>
 						<input
 							type='text'
